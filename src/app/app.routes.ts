13cc1ffb3b6acf73a01f2b1ec43f650bd0729253
felipe-u <felipe_uv@outlook.com>
2024-08-12 21:45:00 -0500

    Creating Not Found Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { NewsComponent } from "./news/news.component";
 import { SingleNewsFullComponent } from "./news/single-news/single-news-full/single-news-full.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
     {
@@ -17,5 +18,9 @@ export const routes: Routes = [
     {
         path: 'news/:newsId',
         component: SingleNewsFullComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ]
